34c7a767c83f4476589331d34bc14c1d3f8e4347
MgAl2O4 <51430403+MgAl2O4@users.noreply.github.com>
2021-08-15 07:28:44 -0400

    branch cleanup

--- ml/patternMatch/run-everything.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set ToolPath=%localappdata%/Programs/Python/Python38
-
-for %%i in (train-*.py) do %ToolPath%/python %%i
-
-pause
--- ml/patternMatch/train-cactpot.py
+++
@@ -1,4 +0,0 @@
-from nn import NNTraining
-
-training = NNTraining(inputFile='ml-cactpot.json', outputFile='ml-cactpot.txt')
-training.run(numHidden1=80, numEpochs=500)
