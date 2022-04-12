26b8f5f97145eff79478f612242826283a7a374e
MgAl2O4 <51430403+MgAl2O4@users.noreply.github.com>
2022-04-11 23:01:53 -0400

    updated for 6.1

--- sources/Properties/AssemblyInfo.cs
+++ sources/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("104.0.0.0")]
-[assembly: AssemblyFileVersion("104.0.0.0")]
+[assembly: AssemblyVersion("105.0.0.0")]
+[assembly: AssemblyFileVersion("105.0.0.0")]
